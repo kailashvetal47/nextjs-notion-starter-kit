1c4287c6eefef50b1426388b51ac825338e6cc3c
kailashvetal47 <kailash.vetal47@gmail.com>
2022-09-24 14:09:33 -0700

    Update - Adds icon

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,20 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // https://kv47.notion.site/KV-s-Digital-Garden-31565fa59a5d42ab964cc77077ec1fec
+  rootNotionPageId: '31565fa59a5d42ab964cc77077ec1fec',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'KVs Digital Garden',
+  domain: 'notion.kv47.dev',
+  author: 'Kailash Vetal',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'KVs Digital Garden',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
